8f9a1db1df74ed116b54d3102dbeb78f5f7e2b81
miyagawananako <miyagawa775@gmail.com>
2025-12-15 22:07:05 +0900

    Refactor NeuralWani: use DList for O(1) concat and Map for fast word lookup

--- src/NeuralWani/SplitJudgment.hs
+++ src/NeuralWani/SplitJudgment.hs
@@ -9,10 +9,14 @@ module NeuralWani.SplitJudgment
     , Token(..)
     , splitJudgment
     , DelimiterToken(..)
+    , dttruleToRuleLabel
+    , buildWordMap
+    , WordMap
     ) where
 
 import qualified DTS.QueryTypes as QT
 import qualified DTS.DTTdeBruijn as U
+import qualified DTS.Prover.Wani.BackwardRules as BR
 import qualified Data.ByteString as B --bytestring
 import qualified Data.Text.Lazy as T  --text
 import Data.Store (decode)
@@ -21,6 +25,8 @@ import qualified Data.List as List
 import Data.Ord
 import qualified Data.Set as Set
 import GHC.Generics
+import qualified Data.DList as DL
+import Data.DList (DList)
 
 loadActionsFromBinary :: FilePath -> IO [(U.Judgment, QT.DTTrule)]
 loadActionsFromBinary filepath = do
@@ -88,139 +94,148 @@ data Token =  FST | SND | COMMA | EOPair | EOPre | EOSig | EOCon | EOTerm | EOTy
             | Type' | Kind' | Pi' | Lam' | App' | Not' | Sigma' | Pair' | Proj' | Disj' | Iota' | Unpack' | Bot' | Unit' | Top' | Entity' | Nat' | Zero' | Succ' | Natrec' | Eq' | Refl' | Idpeel'
   deriving (Enum, Show, Bounded, Eq, Ord)
 
-textToToken :: T.Text -> [T.Text] -> [Token]
-textToToken text frequentWords =
-  case () of
-    _ | length frequentWords > 0 && text == frequentWords !! 0 -> [Word1]
-      | length frequentWords > 1 && text == frequentWords !! 1 -> [Word2]
-      | length frequentWords > 2 && text == frequentWords !! 2 -> [Word3]
-      | length frequentWords > 3 && text == frequentWords !! 3 -> [Word4]
-      | length frequentWords > 4 && text == frequentWords !! 4 -> [Word5]
-      | length frequentWords > 5 && text == frequentWords !! 5 -> [Word6]
-      | length frequentWords > 6 && text == frequentWords !! 6 -> [Word7]
-      | length frequentWords > 7 && text == frequentWords !! 7 -> [Word8]
-      | length frequentWords > 8 && text == frequentWords !! 8 -> [Word9]
-      | length frequentWords > 9 && text == frequentWords !! 9 -> [Word10]
-      | length frequentWords > 10 && text == frequentWords !! 10 -> [Word11]
-      | length frequentWords > 11 && text == frequentWords !! 11 -> [Word12]
-      | length frequentWords > 12 && text == frequentWords !! 12 -> [Word13]
-      | length frequentWords > 13 && text == frequentWords !! 13 -> [Word14]
-      | length frequentWords > 14 && text == frequentWords !! 14 -> [Word15]
-      | length frequentWords > 15 && text == frequentWords !! 15 -> [Word16]
-      | length frequentWords > 16 && text == frequentWords !! 16 -> [Word17]
-      | length frequentWords > 17 && text == frequentWords !! 17 -> [Word18]
-      | length frequentWords > 18 && text == frequentWords !! 18 -> [Word19]
-      | length frequentWords > 19 && text == frequentWords !! 19 -> [Word20]
-      | length frequentWords > 20 && text == frequentWords !! 20 -> [Word21]
-      | length frequentWords > 21 && text == frequentWords !! 21 -> [Word22]
-      | length frequentWords > 22 && text == frequentWords !! 22 -> [Word23]
-      | length frequentWords > 23 && text == frequentWords !! 23 -> [Word24]
-      | length frequentWords > 24 && text == frequentWords !! 24 -> [Word25]
-      | length frequentWords > 25 && text == frequentWords !! 25 -> [Word26]
-      | length frequentWords > 26 && text == frequentWords !! 26 -> [Word27]
-      | length frequentWords > 27 && text == frequentWords !! 27 -> [Word28]
-      | length frequentWords > 28 && text == frequentWords !! 28 -> [Word29]
-      | length frequentWords > 29 && text == frequentWords !! 29 -> [Word30]
-      | length frequentWords > 30 && text == frequentWords !! 30 -> [Word31]
-      | otherwise -> [UNKNOWN]
+-- | テキストをトークンに変換する関数（高速版）
+-- WordMapを使用してO(log n)でルックアップ
+textToTokenDL :: T.Text -> WordMap -> DList Token
+textToTokenDL text wordMap =
+  case Map.lookup text wordMap of
+    Just token -> DL.singleton token
+    Nothing -> DL.singleton UNKNOWN
 
-varToToken :: Int -> [Token]
-varToToken i =
+varToTokenDL :: Int -> DList Token
+varToTokenDL i =
   case i of
-    0 -> [Var'0]
-    1 -> [Var'1]
-    2 -> [Var'2]
-    3 -> [Var'3]
-    4 -> [Var'4]
-    5 -> [Var'5]
-    6 -> [Var'6]
-    _ -> [Var'unknown]
+    0 -> DL.singleton Var'0
+    1 -> DL.singleton Var'1
+    2 -> DL.singleton Var'2
+    3 -> DL.singleton Var'3
+    4 -> DL.singleton Var'4
+    5 -> DL.singleton Var'5
+    6 -> DL.singleton Var'6
+    _ -> DL.singleton Var'unknown
 
-selectorToToken :: U.Selector -> [Token]
-selectorToToken s = case s of
-  U.Fst -> [FST]
-  U.Snd -> [SND]
+selectorToTokenDL :: U.Selector -> DList Token
+selectorToTokenDL s = case s of
+  U.Fst -> DL.singleton FST
+  U.Snd -> DL.singleton SND
 
 data DelimiterToken = Paren | Sep | Eo | Unused deriving (Show, Eq, Enum, Bounded, Ord, Generic, Read)
 
-wrapPreterm :: [Token] -> DelimiterToken -> [Token]
-wrapPreterm xs Paren = [LPAREN] ++ xs ++ [RPAREN]
-wrapPreterm xs Sep = xs ++ [SEP]
-wrapPreterm xs Eo = xs ++ [EOPre]
-wrapPreterm xs Unused = xs
+-- | 頻出語からトークンへのマッピング型
+type WordMap = Map.Map T.Text Token
 
-splitPreterm :: U.Preterm -> [T.Text] -> DelimiterToken -> [Token]
-splitPreterm preterm frequentWords delimiterToken = case preterm of
-  U.Var i -> wrapPreterm (varToToken i) delimiterToken
-  U.Con c -> wrapPreterm (textToToken c frequentWords) delimiterToken
-  U.Type -> wrapPreterm [Type'] delimiterToken
-  U.Kind -> wrapPreterm [Kind'] delimiterToken
-  U.Pi a b -> wrapPreterm ([Pi'] ++ splitPreterm a frequentWords delimiterToken ++ splitPreterm b frequentWords delimiterToken) delimiterToken
-  U.Lam m -> wrapPreterm ([Lam'] ++ splitPreterm m frequentWords delimiterToken) delimiterToken
-  U.App m n -> wrapPreterm ([App'] ++ splitPreterm m frequentWords delimiterToken ++ splitPreterm n frequentWords delimiterToken) delimiterToken
-  U.Not m -> wrapPreterm ([Not'] ++ splitPreterm m frequentWords delimiterToken) delimiterToken
-  U.Sigma a b -> wrapPreterm ([Sigma'] ++ splitPreterm a frequentWords delimiterToken ++ splitPreterm b frequentWords delimiterToken) delimiterToken
-  U.Pair m n -> wrapPreterm ([Pair'] ++ splitPreterm m frequentWords delimiterToken ++ splitPreterm n frequentWords delimiterToken) delimiterToken
-  U.Proj s m -> wrapPreterm ([Proj'] ++ selectorToToken s ++ splitPreterm m frequentWords delimiterToken) delimiterToken
-  U.Disj a b -> wrapPreterm ([Disj'] ++ splitPreterm a frequentWords delimiterToken ++ splitPreterm b frequentWords delimiterToken) delimiterToken
-  U.Iota s m -> wrapPreterm ([Iota'] ++ selectorToToken s ++ splitPreterm m frequentWords delimiterToken) delimiterToken
-  U.Unpack p h m n -> wrapPreterm ([Unpack'] ++ splitPreterm p frequentWords delimiterToken ++ splitPreterm h frequentWords delimiterToken ++ splitPreterm m frequentWords delimiterToken ++ splitPreterm n frequentWords delimiterToken) delimiterToken
-  U.Bot -> wrapPreterm [Bot'] delimiterToken
-  U.Unit -> wrapPreterm [Unit'] delimiterToken
-  U.Top -> wrapPreterm [Top'] delimiterToken
-  U.Entity -> wrapPreterm [Entity'] delimiterToken
-  U.Nat -> wrapPreterm [Nat'] delimiterToken
-  U.Zero -> wrapPreterm [Zero'] delimiterToken
-  U.Succ m -> wrapPreterm ([Succ'] ++ splitPreterm m frequentWords delimiterToken) delimiterToken
-  U.Natrec p n e f -> wrapPreterm ([Natrec'] ++ splitPreterm p frequentWords delimiterToken ++ splitPreterm n frequentWords delimiterToken ++ splitPreterm e frequentWords delimiterToken ++ splitPreterm f frequentWords delimiterToken) delimiterToken
-  U.Eq a m n -> wrapPreterm ([Eq'] ++ splitPreterm a frequentWords delimiterToken ++ splitPreterm m frequentWords delimiterToken ++ splitPreterm n frequentWords delimiterToken) delimiterToken
-  U.Refl a m -> wrapPreterm ([Refl'] ++ splitPreterm a frequentWords delimiterToken ++ splitPreterm m frequentWords delimiterToken) delimiterToken
-  U.Idpeel p e r -> wrapPreterm ([Idpeel'] ++ splitPreterm p frequentWords delimiterToken ++ splitPreterm e frequentWords delimiterToken ++ splitPreterm r frequentWords delimiterToken) delimiterToken
+-- | 頻出語リストからWordMapを構築する関数（事前に一度だけ実行）
+-- O(n log n)で構築し、後のルックアップをO(log n)にする
+buildWordMap :: [T.Text] -> WordMap
+buildWordMap frequentWords = 
+  Map.fromList $ zip frequentWords wordTokens
+  where
+    wordTokens = [Word1, Word2, Word3, Word4, Word5, Word6, Word7, Word8, Word9, Word10,
+                  Word11, Word12, Word13, Word14, Word15, Word16, Word17, Word18, Word19, Word20,
+                  Word21, Word22, Word23, Word24, Word25, Word26, Word27, Word28, Word29, Word30, Word31]
 
-splitPreterms:: [U.Preterm] -> [T.Text] -> DelimiterToken -> [Token]
-splitPreterms preterms frequentWords delimiterToken = concatMap (\preterm -> splitPreterm preterm frequentWords delimiterToken) preterms
+-- | DList版のwrap関数（O(1)での連結）
+wrapPretermDL :: DList Token -> DelimiterToken -> DList Token
+wrapPretermDL xs Paren = DL.singleton LPAREN <> xs <> DL.singleton RPAREN
+wrapPretermDL xs Sep = xs <> DL.singleton SEP
+wrapPretermDL xs Eo = xs <> DL.singleton EOPre
+wrapPretermDL xs Unused = xs
 
-wrapPair :: [Token] -> DelimiterToken -> [Token]
-wrapPair xs Paren = [LPAREN] ++ xs ++ [RPAREN]
-wrapPair xs Sep = xs ++ [SEP]
-wrapPair xs Eo = xs ++ [EOPair]
-wrapPair xs Unused = xs
+-- | DList版のsplitPreterm（O(1)での連結）
+splitPretermDL :: U.Preterm -> WordMap -> DelimiterToken -> DList Token
+splitPretermDL preterm wordMap delimiterToken = case preterm of
+  U.Var i -> wrapPretermDL (varToTokenDL i) delimiterToken
+  U.Con c -> wrapPretermDL (textToTokenDL c wordMap) delimiterToken
+  U.Type -> wrapPretermDL (DL.singleton Type') delimiterToken
+  U.Kind -> wrapPretermDL (DL.singleton Kind') delimiterToken
+  U.Pi a b -> wrapPretermDL (DL.singleton Pi' <> splitPretermDL a wordMap delimiterToken <> splitPretermDL b wordMap delimiterToken) delimiterToken
+  U.Lam m -> wrapPretermDL (DL.singleton Lam' <> splitPretermDL m wordMap delimiterToken) delimiterToken
+  U.App m n -> wrapPretermDL (DL.singleton App' <> splitPretermDL m wordMap delimiterToken <> splitPretermDL n wordMap delimiterToken) delimiterToken
+  U.Not m -> wrapPretermDL (DL.singleton Not' <> splitPretermDL m wordMap delimiterToken) delimiterToken
+  U.Sigma a b -> wrapPretermDL (DL.singleton Sigma' <> splitPretermDL a wordMap delimiterToken <> splitPretermDL b wordMap delimiterToken) delimiterToken
+  U.Pair m n -> wrapPretermDL (DL.singleton Pair' <> splitPretermDL m wordMap delimiterToken <> splitPretermDL n wordMap delimiterToken) delimiterToken
+  U.Proj s m -> wrapPretermDL (DL.singleton Proj' <> selectorToTokenDL s <> splitPretermDL m wordMap delimiterToken) delimiterToken
+  U.Disj a b -> wrapPretermDL (DL.singleton Disj' <> splitPretermDL a wordMap delimiterToken <> splitPretermDL b wordMap delimiterToken) delimiterToken
+  U.Iota s m -> wrapPretermDL (DL.singleton Iota' <> selectorToTokenDL s <> splitPretermDL m wordMap delimiterToken) delimiterToken
+  U.Unpack p h m n -> wrapPretermDL (DL.singleton Unpack' <> splitPretermDL p wordMap delimiterToken <> splitPretermDL h wordMap delimiterToken <> splitPretermDL m wordMap delimiterToken <> splitPretermDL n wordMap delimiterToken) delimiterToken
+  U.Bot -> wrapPretermDL (DL.singleton Bot') delimiterToken
+  U.Unit -> wrapPretermDL (DL.singleton Unit') delimiterToken
+  U.Top -> wrapPretermDL (DL.singleton Top') delimiterToken
+  U.Entity -> wrapPretermDL (DL.singleton Entity') delimiterToken
+  U.Nat -> wrapPretermDL (DL.singleton Nat') delimiterToken
+  U.Zero -> wrapPretermDL (DL.singleton Zero') delimiterToken
+  U.Succ m -> wrapPretermDL (DL.singleton Succ' <> splitPretermDL m wordMap delimiterToken) delimiterToken
+  U.Natrec p n e f -> wrapPretermDL (DL.singleton Natrec' <> splitPretermDL p wordMap delimiterToken <> splitPretermDL n wordMap delimiterToken <> splitPretermDL e wordMap delimiterToken <> splitPretermDL f wordMap delimiterToken) delimiterToken
+  U.Eq a m n -> wrapPretermDL (DL.singleton Eq' <> splitPretermDL a wordMap delimiterToken <> splitPretermDL m wordMap delimiterToken <> splitPretermDL n wordMap delimiterToken) delimiterToken
+  U.Refl a m -> wrapPretermDL (DL.singleton Refl' <> splitPretermDL a wordMap delimiterToken <> splitPretermDL m wordMap delimiterToken) delimiterToken
+  U.Idpeel p e r -> wrapPretermDL (DL.singleton Idpeel' <> splitPretermDL p wordMap delimiterToken <> splitPretermDL e wordMap delimiterToken <> splitPretermDL r wordMap delimiterToken) delimiterToken
 
-splitSignature :: U.Signature -> [T.Text] -> DelimiterToken -> [Token]
-splitSignature signature frequentWords delimiterToken = concatMap (\(name, preterm) -> wrapPair (textToToken name frequentWords ++ [COMMA] ++ splitPreterm preterm frequentWords delimiterToken) delimiterToken) signature
+-- | DList版のsplitPreterms
+splitPretermsDL :: [U.Preterm] -> WordMap -> DelimiterToken -> DList Token
+splitPretermsDL preterms wordMap delimiterToken = 
+  mconcat $ map (\preterm -> splitPretermDL preterm wordMap delimiterToken) preterms
 
-wrapSignature :: [Token] -> DelimiterToken -> [Token]
-wrapSignature xs Paren = [LPAREN] ++ xs ++ [RPAREN]
-wrapSignature xs Sep = xs ++ [SEP]
-wrapSignature xs Eo = xs ++ [EOSig]
-wrapSignature xs Unused = xs
+-- | DList版のwrapPair
+wrapPairDL :: DList Token -> DelimiterToken -> DList Token
+wrapPairDL xs Paren = DL.singleton LPAREN <> xs <> DL.singleton RPAREN
+wrapPairDL xs Sep = xs <> DL.singleton SEP
+wrapPairDL xs Eo = xs <> DL.singleton EOPair
+wrapPairDL xs Unused = xs
 
-wrapContext :: [Token] -> DelimiterToken -> [Token]
-wrapContext xs Paren = [LPAREN] ++ xs ++ [RPAREN]
-wrapContext xs Sep = xs ++ [SEP]
-wrapContext xs Eo = xs ++ [EOCon]
-wrapContext xs Unused = xs
+-- | DList版のsplitSignature
+splitSignatureDL :: U.Signature -> WordMap -> DelimiterToken -> DList Token
+splitSignatureDL signature wordMap delimiterToken = 
+  mconcat $ map (\(name, preterm) -> 
+    wrapPairDL (textToTokenDL name wordMap <> DL.singleton COMMA <> splitPretermDL preterm wordMap delimiterToken) delimiterToken
+  ) signature
 
-wrapTerm ::[Token] -> DelimiterToken -> [Token]
-wrapTerm xs Paren = [LPAREN] ++ xs ++ [RPAREN]
-wrapTerm xs Sep = xs ++ [SEP]
-wrapTerm xs Eo = xs ++ [EOTerm]
-wrapTerm xs Unused = xs
+-- | DList版のwrapSignature
+wrapSignatureDL :: DList Token -> DelimiterToken -> DList Token
+wrapSignatureDL xs Paren = DL.singleton LPAREN <> xs <> DL.singleton RPAREN
+wrapSignatureDL xs Sep = xs <> DL.singleton SEP
+wrapSignatureDL xs Eo = xs <> DL.singleton EOSig
+wrapSignatureDL xs Unused = xs
 
-wrapTyp :: [Token] -> DelimiterToken -> [Token]
-wrapTyp xs Paren = [LPAREN] ++ xs ++ [RPAREN]
-wrapTyp xs Sep = xs ++ [SEP]
-wrapTyp xs Eo = xs ++ [EOTyp]
-wrapTyp xs Unused = xs
+-- | DList版のwrapContext
+wrapContextDL :: DList Token -> DelimiterToken -> DList Token
+wrapContextDL xs Paren = DL.singleton LPAREN <> xs <> DL.singleton RPAREN
+wrapContextDL xs Sep = xs <> DL.singleton SEP
+wrapContextDL xs Eo = xs <> DL.singleton EOCon
+wrapContextDL xs Unused = xs
 
-splitJudgment :: U.Judgment -> [T.Text] -> DelimiterToken -> [Token]
-splitJudgment judgment frequentWords delimiterToken =
+-- | DList版のwrapTerm
+wrapTermDL :: DList Token -> DelimiterToken -> DList Token
+wrapTermDL xs Paren = DL.singleton LPAREN <> xs <> DL.singleton RPAREN
+wrapTermDL xs Sep = xs <> DL.singleton SEP
+wrapTermDL xs Eo = xs <> DL.singleton EOTerm
+wrapTermDL xs Unused = xs
+
+-- | DList版のwrapTyp
+wrapTypDL :: DList Token -> DelimiterToken -> DList Token
+wrapTypDL xs Paren = DL.singleton LPAREN <> xs <> DL.singleton RPAREN
+wrapTypDL xs Sep = xs <> DL.singleton SEP
+wrapTypDL xs Eo = xs <> DL.singleton EOTyp
+wrapTypDL xs Unused = xs
+
+-- | DList版のsplitJudgment（内部実装）
+splitJudgmentDL :: U.Judgment -> WordMap -> DelimiterToken -> DList Token
+splitJudgmentDL judgment wordMap delimiterToken =
   if isIncludeTerm
-    then  wrapSignature (splitSignature (U.signtr judgment) frequentWords delimiterToken) delimiterToken ++
-          wrapContext (splitPreterms (U.contxt judgment) frequentWords delimiterToken) delimiterToken ++
-          wrapTerm (splitPreterm (U.trm judgment) frequentWords delimiterToken) delimiterToken ++
-          wrapTyp (splitPreterm (U.typ judgment) frequentWords delimiterToken) delimiterToken
-    else  wrapSignature (splitSignature (U.signtr judgment) frequentWords delimiterToken) delimiterToken ++
-          wrapContext (splitPreterms (U.contxt judgment) frequentWords delimiterToken) delimiterToken ++
-          wrapTyp (splitPreterm (U.typ judgment) frequentWords delimiterToken) delimiterToken
+    then  wrapSignatureDL (splitSignatureDL (U.signtr judgment) wordMap delimiterToken) delimiterToken <>
+          wrapContextDL (splitPretermsDL (U.contxt judgment) wordMap delimiterToken) delimiterToken <>
+          wrapTermDL (splitPretermDL (U.trm judgment) wordMap delimiterToken) delimiterToken <>
+          wrapTypDL (splitPretermDL (U.typ judgment) wordMap delimiterToken) delimiterToken
+    else  wrapSignatureDL (splitSignatureDL (U.signtr judgment) wordMap delimiterToken) delimiterToken <>
+          wrapContextDL (splitPretermsDL (U.contxt judgment) wordMap delimiterToken) delimiterToken <>
+          wrapTypDL (splitPretermDL (U.typ judgment) wordMap delimiterToken) delimiterToken
 
+-- | splitJudgment（公開インターフェース）
+-- 内部でDListを使用し、最後にリストに変換
+splitJudgment :: U.Judgment -> WordMap -> DelimiterToken -> [Token]
+splitJudgment judgment wordMap delimiterToken = 
+  DL.toList $ splitJudgmentDL judgment wordMap delimiterToken
+
+-- | DTTruleからRuleLabelへの変換関数
+-- lightblueのBR.dttruleToRuleLabelをエクスポート
+dttruleToRuleLabel :: QT.DTTrule -> Maybe BR.RuleLabel
+dttruleToRuleLabel = BR.dttruleToRuleLabel
