fd5cc995f0b8f43de4a7fbb4437859a54948a2bb
nanacom <miyagawa775@gmail.com>
2025-12-17 15:39:07 +0900

    fix

--- src/NeuralWani/Builder.hs
+++ src/NeuralWani/Builder.hs
@@ -12,7 +12,6 @@ import qualified DTS.QueryTypes as QT
 import qualified Data.Text.Lazy as T
 import qualified Data.ByteString as B --bytestring
 import Data.Store (decode)
-import Data.List (nub)
 
 import Torch.Serialize (loadParams)
 import Torch.NN (sample)
@@ -23,35 +22,35 @@ import qualified NeuralWani.SplitJudgment as S
 import qualified Data.Map.Strict as Map
 
 -- 本来lightblue内に置くパス（パスは仮）。CUDA でしか開けない
--- modelPath :: FilePath
--- modelPath = "trainedDataWithoutF/typeEo_biFalse_s32_lr5.0e-4_i256_h256_layer1/2025-12-10_20-46-49/seq-class.model"
--- frequentWordsPath :: FilePath
--- frequentWordsPath = "trainedDataWithoutF/typeEo_biFalse_s32_lr5.0e-4_i256_h256_layer1/2025-12-10_20-46-49/frequentWords.bin"
+modelPath :: FilePath
+modelPath = "trainedDataBackwardWithoutF/typeEo_biTrue_s32_lr5.0e-4_i256_h256_layer1/2025-12-17_09-26-43/seq-class.model"
+frequentWordsPath :: FilePath
+frequentWordsPath = "trainedDataBackwardWithoutF/typeEo_biTrue_s32_lr5.0e-4_i256_h256_layer1/2025-12-17_09-26-43/frequentWords.bin"
 
 -- CPU用のパス
-modelPath :: FilePath
-modelPath = "trainedDataBackwardWithoutF/typeUnused_biFalse_s32_lr5.0e-4_i256_h256_layer1/2025-12-15_12-14-54/seq-class.model"
-frequentWordsPath :: FilePath
-frequentWordsPath = "trainedDataBackwardWithoutF/typeUnused_biFalse_s32_lr5.0e-4_i256_h256_layer1/2025-12-15_12-14-54/frequentWords.bin"
+-- modelPath :: FilePath
+-- modelPath = "trainedDataBackwardWithoutF/typeUnused_biFalse_s32_lr5.0e-4_i256_h256_layer1/2025-12-15_12-14-54/seq-class.model"
+-- frequentWordsPath :: FilePath
+-- frequentWordsPath = "trainedDataBackwardWithoutF/typeUnused_biFalse_s32_lr5.0e-4_i256_h256_layer1/2025-12-15_12-14-54/frequentWords.bin"
 
 
 -- lightblue内に置く関数
 neuralWaniBuilder :: IO (WB.Goal -> [BR.RuleLabel] -> [BR.RuleLabel])
 neuralWaniBuilder = do
-  let device = Device CPU 0
+  let device = Device CUDA 0
+      bi_directional = True
       hyperParams = F.HypParams
         { F.dev = device
-        , F.bi_directional = False
+        , F.bi_directional = bi_directional
         , F.emb_dim = 256
-        , F.has_bias = False  -- 訓練時と同じ値に設定
+        , F.has_bias = True  -- 訓練時と同じ値に設定
         , F.proj_size = Nothing
         , F.vocab_size = length (enumFrom minBound :: [S.Token])
         , F.num_layers = 1
         , F.hidden_size = 256
         , F.num_rules = length (enumFrom minBound :: [BR.RuleLabel])
         }
-      bi_directional = False
-      delimiterToken = S.Unused
+      delimiterToken = S.Eo
   emptyModel <- sample hyperParams
   model <- loadParams emptyModel modelPath
   frequentWordsEither <- decode <$> B.readFile frequentWordsPath
@@ -65,6 +64,6 @@ neuralWaniBuilder = do
     in case maybeJudgment of
       Just judgment ->
         let predictedRuleLabels = F.predictRule device model judgment bi_directional wordMap delimiterToken
-            filteredRuleLabels = nub $ filter (`elem` availableRuleLabels) predictedRuleLabels
+            filteredRuleLabels = filter (`elem` availableRuleLabels) predictedRuleLabels
         in filteredRuleLabels
       Nothing -> availableRuleLabels
