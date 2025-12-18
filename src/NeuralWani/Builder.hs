7ec06f0f2d688ca5ddd403846224d678046df1f1
nanacom <miyagawa775@gmail.com>
2025-12-18 18:11:34 +0900

    lazy evaluate getPrioritizedRules

--- src/NeuralWani/Builder.hs
+++ src/NeuralWani/Builder.hs
@@ -23,9 +23,9 @@ import qualified Data.Map.Strict as Map
 
 -- 本来lightblue内に置くパス（パスは仮）。CUDA でしか開けない
 modelPath :: FilePath
-modelPath = "trainedDataBackwardWithoutF/typeEo_biTrue_s32_lr5.0e-4_i256_h256_layer1/2025-12-17_09-26-43/seq-class.model"
+modelPath = "trainedDataBackwardWithoutF/typeEo_biFalse_s32_lr5.0e-4_i128_h128_layer1/2025-12-16_13-41-38/seq-class.model"
 frequentWordsPath :: FilePath
-frequentWordsPath = "trainedDataBackwardWithoutF/typeEo_biTrue_s32_lr5.0e-4_i256_h256_layer1/2025-12-17_09-26-43/frequentWords.bin"
+frequentWordsPath = "trainedDataBackwardWithoutF/typeEo_biFalse_s32_lr5.0e-4_i128_h128_layer1/2025-12-16_13-41-38/frequentWords.bin"
 
 -- CPU用のパス
 -- modelPath :: FilePath
@@ -38,16 +38,16 @@ frequentWordsPath = "trainedDataBackwardWithoutF/typeEo_biTrue_s32_lr5.0e-4_i256
 neuralWaniBuilder :: IO (WB.Goal -> [BR.RuleLabel] -> [BR.RuleLabel])
 neuralWaniBuilder = do
   let device = Device CUDA 0
-      bi_directional = True
+      bi_directional = False
       hyperParams = F.HypParams
         { F.dev = device
         , F.bi_directional = bi_directional
-        , F.emb_dim = 256
+        , F.emb_dim = 128
         , F.has_bias = True  -- 訓練時と同じ値に設定
         , F.proj_size = Nothing
         , F.vocab_size = length (enumFrom minBound :: [S.Token])
         , F.num_layers = 1
-        , F.hidden_size = 256
+        , F.hidden_size = 128
         , F.num_rules = length (enumFrom minBound :: [BR.RuleLabel])
         }
       delimiterToken = S.Eo
